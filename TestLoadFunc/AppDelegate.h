79459cd31a330475d96a983af192f514cb6281f8
laixuefei <laixuefei@g7.com.cn>
2018-04-17 15:14:14 +0800

    Initial Commit

---
+++ TestLoadFunc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestLoadFunc
+//
+//  Created by LaiXuefei on 2018/4/17.
+//  Copyright © 2018年 lxf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
